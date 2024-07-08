e6fd2b1d5ba32cdc89c0a0329e882fba9ce110e7
roopsbhasima7 <rupeshbhasima@gmail.com>
2024-07-08 22:10:49 +0545

    JS  Overview

---
+++ JS Overview/script.js
@@ -0,0 +1,29 @@
+// word vs keyword ✅
+
+// kaka = word
+// for = keyword
+// dai = word
+
+// var const let
+
+// variables and constants ✅
+
+// var dulha = "Lab";
+// var dulahi = "Laby";
+
+// hoisting - variable and functions are hoisted which means there declaration is moved on the top of the code. ✅
+
+// console.log(a);
+// var a = 12;
+
+var a; // declaration - moves on top
+a = 12; // initialization
+
+// types in js
+// primitives and reference
+
+// primitives = number, string, null, undefined, boolean
+// reference = [] {} ()
+
+var a = 12;
+var b = a;
