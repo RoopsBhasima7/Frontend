db899c0a24e624058ac8b4866e7e87e69111f372
roopsbhasima7 <rupeshbhasima@gmail.com>
2024-10-24 23:08:14 +0545

    var let const diff in js

---
+++ JS Overview/readme2.md
@@ -0,0 +1,14 @@
+js
+-word vs keyword
+-var const let
+--the difference
+-- window object
+-- browser context api
+-- stack
+-- heap memory
+-- execution context
+-- lexical environment
+-hoisting
+-types in js
+--how to copy reference values
+-conditionals
---
+++ JS Overview/script2.js
@@ -0,0 +1,8 @@
+// difference betwenn var let const
+//var let const
+// var in old js
+// var = function scoped => var can be used in anywhere in it's parent function
+// var adds itself to the window object
+// let const in new js
+// let const = braces scoped
+// let const doesn't add
